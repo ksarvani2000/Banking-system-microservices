97fee4ac623952015a867999a9df6d525c502934
ksarvani2000 <k.sarvani2000@gmail.com>
2022-09-12 11:49:59 +0530

    banking-system-cutomer was created

---
+++ Banking-system-customer/src/main/java/com/example/demo/BankingSystemCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemCustomerApplication.class, args);
+	}
+
+}
