0bd8e1d8115b8b4cb86e0602759439be65f52322
ksarvani2000 <k.sarvani2000@gmail.com>
2022-09-12 12:32:46 +0530

    updated customer

--- Banking-system-customer/src/main/java/com/demo/spring/rest/CustomerRestController.java
+++ Banking-system-customer/src/main/java/com/demo/spring/rest/CustomerRestController.java
@@ -1,9 +1,13 @@
 package com.demo.spring.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.spring.repo.CustomerRepo;
 
 @RestController
 public class CustomerRestController {
-
 	
+	@Autowired
+	CustomerRepo repo;
 }
